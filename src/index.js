7037564a0f3890b36e768355f738e97bb1737f23
Petmel7 <115225282+Petmel7@users.noreply.github.com>
2023-06-18 16:35:01 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import "./style.css";
+import axios from 'axios';
 
 const refs = {
     form: document.querySelector('#form'),
@@ -9,18 +10,20 @@ const refs = {
 const handlerSubmit = (e) => {
     e.preventDefault();
     const value = refs.input.value;
-    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${value}`)
-     .then(response => response.json())
-     .then(coctails => renderCollection(coctails.drinks))
-     .catch(err => console.log(err))
+    // fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${value}`)
+    axios.get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${value}`)
+        .theh(result => console.log(result))
+        // .then(response => response.json())
+        // .then(coctails => renderCollection(coctails.drinks))
+        // .catch(err => console.log(err))
 }
 
-function createItem({ strDrinkThumb, strDrink, strInstructions }) {
+function createItem({ strDrinkThumb, strDrink }) {
     const article = `
         <li>
             <img src="${strDrinkThumb}" 
             alt="${strDrink}">
-            <p>${strInstructions}</p>
+            <p>${strDrink}</p>
         </li>`
     
     refs.jsmarkup.insertAdjacentHTML("beforeend", article);
