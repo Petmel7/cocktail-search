8edd5131651a6f39f19041a4c1f9951335122c7b
Petmel7 <115225282+Petmel7@users.noreply.github.com>
2023-06-19 21:06:46 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import "./style.css";
 import axios from 'axios';
 
+// !Пошук коктейлів
+
 // const refs = {
 //     form: document.querySelector('#form'),
 //     input: document.querySelector('#search'),
@@ -50,12 +52,13 @@ import axios from 'axios';
 
 // refs.form.addEventListener('submit', handlerSubmit);
 
-
+// !Пошук юзерів на GitHub та пагінація
 
 const refs = {
     form: document.querySelector('#form'),
     input: document.querySelector('#search'),
-    jsmarkup: document.querySelector('.jsmarkup')
+    jsmarkup: document.querySelector('.jsmarkup'),
+    more: document.querySelector('#more')
 }
 
 let currentPage = 1;
@@ -67,9 +70,8 @@ const gitHandlerSubmit = (e) => {
     axios.get(`https://api.github.com/search/users?q=${value}&client_id=bc979564c898738507ac&client_secret=91ea2a149d384989e086b6bfd5e6132ac830005c&page=${currentPage}`)
         .then(response => {
             renderGitCollection(response.data.items);
-            clearInput();
         })
-        .theh(() => currentPage++)
+        .then(() => currentPage++)
 
         .catch(error => {
             console.log(error);
@@ -77,23 +79,21 @@ const gitHandlerSubmit = (e) => {
 }
 
 function createGitItem({ avatar_url, login }) {
-    const li = document.createElement('li');
-    li.innerHTML = `
-        <img src="${avatar_url}" 
+    
+    const article =`
+    <li>
+        <img src="${avatar_url}"
         alt="${login}">
         <p>${login}</p>
+    </li>
     `;
     
-    refs.jsmarkup.appendChild(li);
+    refs.jsmarkup.insertAdjacentHTML("beforeend", article);
 }
 
 function renderGitCollection(arr) {
-    refs.jsmarkup.innerHTML = ''; // Очищаємо контейнер перед відображенням нових результатів
     arr.forEach(el => createGitItem(el));
 }
 
-function clearInput() {
-    refs.input.value = ''; // Очищаємо поле вводу
-}
-
 refs.form.addEventListener('submit', gitHandlerSubmit);
+refs.more.addEventListener('click', gitHandlerSubmit);
